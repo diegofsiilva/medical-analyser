169d1b4498ff43afcdd75a6dccece3ffa6953ec0
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-09-05 09:27:41 -0300

    baseline

---
+++ medical_data_visualizer.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('medical_examination.csv')
+
+df['overweight'] = ((df["weight"]/df["height"] /100)**2 ).apply(lambda x: 1 if x>25 else 0)
+
+df['cholesterol'] = df["cholesterol"].apply(lambda x:0 if x==1 else 1)
+
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = None
+
+
+    # 6
+    df_cat = None
+    
+
+    # 7
+
+
+
+    # 8
+    fig = None
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = None
+
+    # 12
+    corr = None
+
+    # 13
+    mask = None
+
+
+
+    # 14
+    fig, ax = None
+
+    # 15
+
+
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
