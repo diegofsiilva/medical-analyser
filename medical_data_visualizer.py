86810cc702425a2c166adcb4bc95535fb6a65bce
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-09-07 13:04:57 -0300

    fix: debug of variables

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -5,30 +5,18 @@ import numpy as np
 
 df = pd.read_csv('medical_examination.csv')
 
-df['overweight'] = ((df["weight"]/df["height"] /100)**2 ).apply(lambda x: 1 if x>25 else 0)
+df['overweight'] = ((df['weight']/df['height'] /100)**2 ).apply(lambda x: 1 if x>25 else 0)
 
-df['cholesterol'] = df["cholesterol"].apply(lambda x:0 if x==1 else 1)
+df['cholesterol'] = df['cholesterol'].apply(lambda x:0 if x==1 else 1)
+df["gluc"] = (df["gluc"].apply(lambda x: 0 if x == 1 else 1))
 
-
-# 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-
-    # 6
-    df_cat = None
-    
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    df_cat["total"]=1
+    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).count()
+    # o as_index vai retornar os objetos como um grupo labels    
+    fig = sns.catplot(x="variable", y="total", data=df_cat, hue="value",kind="bar",col="cardio").fig
     fig.savefig('catplot.png')
     return fig
 
