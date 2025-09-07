f3643bc421e6ad5e679d9885a6f41868868685f5
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-09-07 14:11:26 -0300

    fix: debug of function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from unittest import main
 # Test your function by calling it here
 medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
+medical_data_visualizer.draw_scatter_plot()
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,44 +3,88 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('medical_examination.csv')
+# Import data
+df = pd.read_csv("medical_examination.csv")
 
-df['overweight'] = ((df['weight']/df['height'] /100)**2 ).apply(lambda x: 1 if x>25 else 0)
+# Add 'overweight' column
+df['overweight'] = (
+    df["weight"] / ((df["height"] / 100) ** 2)
+).apply(lambda x: 1 if x > 25 else 0)
 
-df['cholesterol'] = df['cholesterol'].apply(lambda x:0 if x==1 else 1)
-df["gluc"] = (df["gluc"].apply(lambda x: 0 if x == 1 else 1))
+# Normalize data by making 0 always good and 1 always bad
+df["cholesterol"] = df["cholesterol"].apply(lambda x: 0 if x == 1 else 1)
+df["gluc"] = df["gluc"].apply(lambda x: 0 if x == 1 else 1)
 
+# Draw Categorical Plot
 def draw_cat_plot():
-    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    # Create DataFrame for cat plot using `pd.melt`
+    df_cat = pd.melt(
+        df,
+        id_vars=["cardio"],
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    )
 
-    df_cat["total"]=1
+    # Group and reformat the data to split it by 'cardio'
+    df_cat["total"] = 1
     df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).count()
-    # o as_index vai retornar os objetos como um grupo labels    
-    fig = sns.catplot(x="variable", y="total", data=df_cat, hue="value",kind="bar",col="cardio").fig
+
+    # Draw the catplot with `sns.catplot()`
+    fig = sns.catplot(
+        x="variable",
+        y="total",
+        data=df_cat,
+        hue="value",
+        kind="bar",
+        col="cardio"
+    ).fig
+
+    # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
-
-# 10
+# Draw Heat Map
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Clean the data
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
-    # 12
-    corr = None
+    # Calculate the correlation matrix
+    corr = df_heat.corr(method="pearson")
 
-    # 13
-    mask = None
+    # Generate a mask for the upper triangle
+    mask = np.triu(corr)
 
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12, 12))
 
+    # Draw the heatmap with `sns.heatmap()`
+    sns.heatmap(
+        corr,
+        linewidths=1,
+        annot=True,
+        square=True,
+        mask=mask,
+        fmt=".1f",
+        center=0.08,
+        cbar_kws={"shrink": 0.5}
+    )
 
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    # Do not modify the next two lines
     fig.savefig('heatmap.png')
+    return fig
+
+# Draw Scatter Plot
+def draw_scatter_plot():
+    fig = sns.relplot(
+        x="weight",
+        y="height",
+        hue="active",
+        data=df
+    )
+    fig.savefig('scatterplot.png')
     return fig
